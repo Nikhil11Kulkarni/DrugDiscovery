b0252aa107e47cd05c30b22141de0fc82654fd29
ankitakash2007 <ankitakash2007@gmail.com>
2018-10-01 01:09:57 +0530

    Refactoring the code

---
+++ b.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+## This is to run the first program main.cpp
+minisat test1.satinput test1.satoutput
---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## This is to run the first program main.cpp
+rm m1
+rm m2
+g++ -o m1 main.cpp -std=c++14
+g++ -o m2 main2.cpp -std=c++14
